a62c952057f0a7df82371b2de351f730e37c21aa
DESKTOP-C6AAJ1N\KTiger <ktiger981015@outlook.com>
2022-10-06 13:28:09 -0700

    Initial project for Web API using .NET 6.09

---
+++ WebAPITest/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using WebAPITest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _dbContext;
+
+        public UsersController(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            if (_dbContext.Users == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            if (_dbContext.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _dbContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool UserExists(long id)
+        {
+            return (_dbContext.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
